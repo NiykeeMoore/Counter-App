ba5a651874f9483d5aa71275d4187fa3d8d08dff
Niykee Moore <cocacolalocal@ya.ru>
2024-12-16 14:08:23 +0300

    refactor: рефактор логики обработки кнопки и имени кнопок

---
+++ Counter/Model.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  Counter
+//
+//  Created by Niykee Moore on 16.12.2024.
+//
+
+import Foundation
+
+enum ButtonName: String {
+    case increment = "+"
+    case decrement = "-"
+    case resetData = "🔄"
+}
